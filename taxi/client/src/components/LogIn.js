33e6ef3b2035665c2781e2b2b71f40438e4463a3
Igor Znamensky <igor.znamensky@gmail.com>
2021-01-10 12:01:35 +0300

    Done: Part 2, Chapter 3, React Bootstrap Next: Part 2, Chapter 6, Sign Up

--- taxi/client/src/components/LogIn.js
+++ taxi/client/src/components/LogIn.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Breadcrumb, Card, Col, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Formik } from 'formik';
+import { Breadcrumb, Button, Card, Col, Form, Row} from 'react-bootstrap';
 
 function LogIn (props) {
+    /*
+    * We added a hook to set the isSubmitted state,
+    * and we added a conditional statement to redirect
+    * the browser to the home page if the form is submitted.
+    * */
+    const [isSubmitted, setSubmitted] = useState(false);
+
+    /*
+    * We also defined a simple onSubmit() function that sets isSubmitted to true when invoked.
+    * */
+    const onSubmit = (values, actions) => setSubmitted(true);
+
+    if (isSubmitted) {
+        return <Redirect to='/' />;
+    }
+
     return (
         <Row>
             <Col lg={12}>
@@ -12,7 +29,42 @@ function LogIn (props) {
                 </Breadcrumb>
                 <Card>
                     <Card.Header>Log in</Card.Header>
-                    <Card.Body></Card.Body>
+                    <Card.Body>
+                        <Formik
+                            initialValues={{
+                                username: '',
+                                password: ''
+                            }}
+                            onSubmit={onSubmit}
+                        >
+                            {({
+                                  handleChange,
+                                  handleSubmit,
+                                  values
+                              }) => (
+                                <Form noValidate onSubmit={handleSubmit}>
+                                    <Form.Group controlId='username'>
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control
+                                            name='username'
+                                            onChange={handleChange}
+                                            value={values.username}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId='password'>
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control
+                                            name='password'
+                                            onChange={handleChange}
+                                            type='password'
+                                            value={values.password}
+                                        />
+                                    </Form.Group>
+                                    <Button block type='submit' variant='primary'>Log in</Button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Card.Body>
                     <p className='mt-3 text-center'>
                         Don't have an account? <Link to='/sign-up'>Sign up!</Link>
                     </p>
