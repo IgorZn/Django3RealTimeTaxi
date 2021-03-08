35f0b5154fc6320c7dcc9e8ac238ddcd0b3c6fb6
Igor Znamensky <igor.znamensky@gmail.com>
2021-03-08 16:31:38 +0300

    Done: Part 3, Chapter 2, Dashboard Next: Part 3, Chapter 2, Refactoring: Reusable Functions

--- taxi/client/cypress/integration/rider.spec.js
+++ taxi/client/cypress/integration/rider.spec.js
@@ -36,5 +36,26 @@ describe('The rider dashboard', function () {
 
         cy.visit('/#/rider')
         cy.hash().should('eq', '#/')
-    })
+    });
+
+    it('Can be visited if the user is a rider', function () {
+        const { username, password } = Cypress.env('rider')
+
+        // Capture API calls.
+        cy.server()
+        cy.route('POST', '**/api/log_in/').as('logIn')
+
+        // Log in.
+        cy.visit('/#/log-in')
+        cy.get('input#username').type(username)
+        cy.get('input#password').type(password, { log: false })
+        cy.get('button').contains('Log in').click()
+        cy.hash().should('eq', '#/')
+        cy.get('button').contains('Log out')
+        cy.wait('@logIn')
+
+        cy.visit('/#/rider')
+        cy.hash().should('eq', '#/rider')
+    });
+
 })
