867362973429a25f8d3f91dfbdbaf0fdd8ccb8cc
Igor Znamensky <igor.znamensky@gmail.com>
2021-03-13 10:31:16 +0300

    Done: Part 3, Chapter 3, The Rider Next: Part 3, Chapter 3, The Driver

--- taxi/client/src/components/AuthService.js
+++
@@ -1,7 +0,0 @@
-export const getAccessToken = () => {
-    const auth = JSON.parse(window.localStorage.getItem('taxi.auth'));
-    if (auth) {
-        return auth.access;
-    }
-    return undefined;
-};
--- taxi/client/src/components/Rider.js
+++ taxi/client/src/components/Rider.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Breadcrumb, Card, Col, Row } from 'react-bootstrap';
 
+import TripCard from './TripCard';
 import { Redirect } from 'react-router-dom';
 import { isRider } from '../services/AuthService';
+import { getTrips } from '../services/TripService'; // new
+
+
 
 function Rider (props) {
+    const [trips, setTrips] = useState([]);
+
+    const getCurrentTrips = () => {
+        return trips.filter(trip => {
+            return (
+                trip.driver !== null &&
+                trip.status !== 'REQUESTED' &&
+                trip.status !== 'COMPLETED'
+            );
+        });
+    };
+
+    const getCompletedTrips = () => {
+        return trips.filter(trip => {
+            return trip.status === 'COMPLETED';
+        });
+    };
+
+    useEffect(() => {
+        const loadTrips = async () => {
+            const { response, isError } = await getTrips();
+            if (isError) {
+                setTrips([]);
+            } else {
+                setTrips(response.data);
+            }
+        }
+        loadTrips();
+    }, []);
+
     if (!isRider()) {
         return <Redirect to='/' />
     }
@@ -16,18 +50,18 @@ function Rider (props) {
                     <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
                     <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
                 </Breadcrumb>
-                <Card className='mb-3'>
-                    <Card.Header>Current Trip</Card.Header>
-                    <Card.Body>
-                        No trips.
-                    </Card.Body>
-                </Card>
-                <Card className='mb-3'>
-                    <Card.Header>Recent Trips</Card.Header>
-                    <Card.Body>
-                        No trips.
-                    </Card.Body>
-                </Card>
+                <TripCard
+                    title='Current Trip'
+                    trips={getCurrentTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
+                <TripCard
+                    title='Recent Trips'
+                    trips={getCompletedTrips()}
+                    group='rider'
+                    otherGroup='driver'
+                />
             </Col>
         </Row>
     );
