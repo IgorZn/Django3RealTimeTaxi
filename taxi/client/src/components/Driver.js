8a5231e0854206a4e40d0b6bd3da80e7d4e956f8
Igor Znamensky <igor.znamensky@gmail.com>
2021-04-25 17:44:31 +0300

    Done: Part 3, Chapter 5, Trip Detail View Next: Part 3, Chapter 5, Routing Changes (//components/RiderDashboard.js)

--- taxi/client/src/components/Driver.js
+++ taxi/client/src/components/Driver.js
@@ -1,80 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Breadcrumb, Col, Row } from 'react-bootstrap';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-import { Redirect } from 'react-router-dom';
-import TripCard from './TripCard';
-import { getTrips } from '../services/TripService';
+import DriverDashboard from './DriverDashboard';
+import DriverDetail from './DriverDetail';
 import { isDriver } from '../services/AuthService';
 
 function Driver (props) {
-
-    // HELPERS
-    const [trips, setTrips] = useState([]);
-
-    useEffect(() => {
-        const loadTrips = async () => {
-            const { response, isError } = await getTrips();
-            if (isError) {
-                setTrips([]);
-            } else {
-                setTrips(response.data);
-            }
-        }
-        loadTrips();
-    }, []);
-
-    const getCurrentTrips = () => {
-        return trips.filter(trip => {
-            return trip.driver !== null && trip.status !== 'COMPLETED';
-        });
-    }
-
-    const getRequestedTrips = () => {
-        return trips.filter(trip => {
-            return trip.status === 'REQUESTED';
-        });
-    }
-
-    const getCompletedTrips = () => {
-        return trips.filter(trip => {
-            return trip.status === 'COMPLETED';
-        });
-    }
-
-
-
-
     if (!isDriver()) {
         return <Redirect to='/' />
     }
 
     return (
-        <Row>
-            <Col lg={12}>
-                <Breadcrumb>
-                    <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item active>Dashboard</Breadcrumb.Item>
-                </Breadcrumb>
-                <TripCard
-                    title='Current Trip'
-                    trips={getCurrentTrips()}
-                    group='driver'
-                    otherGroup='rider'
-                />
-                <TripCard
-                    title='Requested Trips'
-                    trips={getRequestedTrips()}
-                    group='driver'
-                    otherGroup='rider'
-                />
-                <TripCard
-                    title='Recent Trips'
-                    trips={getCompletedTrips()}
-                    group='driver'
-                    otherGroup='rider'
-                />
-            </Col>
-        </Row>
+        <Switch>
+            <Route path='/driver/:id' component={DriverDetail} />
+            <Route component={DriverDashboard} />
+        </Switch>
     );
 }
 
